adb4030d243ad8beb269234bb354fc49dfdc6035
masoudparsa <m.parsanejad@gmail.com>
2016-06-14 13:45:32 +0430

    test

---
+++ MVCTypeScriptObjects/Controllers/ObjectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTypeScriptObjects.Controllers
+{
+    public class ObjectsController : Controller
+    {
+        // GET: Objects
+        public ActionResult Objects()
+        {
+            return View();
+        }
+    }
+}
